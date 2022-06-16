dfcbe0433986d59ef5cf180ac471a6989eccaeac
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-06-16 12:27:07 -0600

    ✨ Feat(api): Add error handler middleware

---
+++ api/src/middleware/error-handler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { ServerError } from "../errors/server-error";
+import Logger from '../logger';
+
+export async function errorHandler(
+	error: ServerError,
+	req: Request,
+	res: Response,
+	_next: NextFunction,
+): Promise<void> {
+	Logger.log(`[errorHandler]:: request ${req.method} ${req.originalUrl} :: ${error.message}`, { ...error });
+
+	res.status(error.status || 500).json({
+		error: {
+			message: error.message,
+			code: error.code,
+		},
+	});
+}
