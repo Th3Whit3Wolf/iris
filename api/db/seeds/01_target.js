d72c2a539d66d9d59a69d6aca2c43ee1921c6729
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-06-16 12:12:09 -0600

    🌱 Refactor(seeds): Change table names
    This commit does the following:
    
    - Change table names to reflect the changes in previous commit
    - Makes seed functions fat arrow functions
    - Removes all manual id setting

--- api/db/seeds/01_target.js
+++ api/db/seeds/01_target.js
@@ -1,21 +1,21 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async (knex) => {  
   // Deletes ALL existing entries
   await knex.schema.raw('TRUNCATE target CASCADE');
-  await knex('target').del()
-  await knex('target').insert([
-    {id: 1, name: 'ARKE 3G', offset: 400},
-    {id: 2, name: 'AURORA 2B', offset: 450},
-    {id: 3, name: 'AUXO STAR', offset: 420},
-    {id: 4, name: 'ENYO', offset: 300},
-    {id: 5, name: 'HASHCOMM 7', offset: 365},
-    {id: 6, name: 'HUF UHF FO', offset: 210},
-    {id: 7, name: 'MERCURY PAWN', offset: 150},
-    {id: 8, name: 'NYXSAT', offset: 250},
-    {id: 9, name: 'RASCAL', offset: 120},
-    {id: 10, name: 'WILL 1-AM', offset: 345},
+  await knex('targets').del();
+  await knex('targets').insert([
+    { name: 'ARKE 3G', offset: 400 },
+    { name: 'AURORA 2B', offset: 450 },
+    { name: 'AUXO STAR', offset: 420 },
+    { name: 'ENYO', offset: 300 },
+    { name: 'HASHCOMM 7', offset: 365 },
+    { name: 'HUF UHF FO', offset: 210 },
+    { name: 'MERCURY PAWN', offset: 150 },
+    { name: 'NYXSAT', offset: 250 },
+    { name: 'RASCAL', offset: 120 },
+    { name: 'WILL 1-AM', offset: 345 },
   ]);
 };
--- api/db/seeds/initial_app_author.js
+++ api/db/seeds/initial_app_author.js
@@ -2,13 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.seed = async function (knex) {
+exports.seed = async (knex) => { 
   await knex('app_authors').del();
-  await knex("app_authors").insert([
-    { first_name: "Collin", last_name: "Gilmore" },
-    { first_name: "Shane", last_name: "Askins" },
-    { first_name: "Theodore", last_name: "Kruczek" },
-    { first_name: "Brett", last_name: "Peters" },
-    { first_name: "Brandon", last_name: "Hufstetler" },
+  await knex('app_authors').insert([
+    { first_name: 'Collin', last_name: 'Gilmore' },
+    { first_name: 'Shane', last_name: 'Askins' },
+    { first_name: 'Theodore', last_name: 'Kruczek' },
+    { first_name: 'Brett', last_name: 'Peters' },
+    { first_name: 'Brandon', last_name: 'Hufstetler' },
   ]);
 };
