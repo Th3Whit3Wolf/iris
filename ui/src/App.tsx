0817668e1878c05457a1a6f8a8a4818c5a8e487d
David Karrick <the.white.wolf.is.1337@gmail.com>
2022-06-29 07:19:47 -0600

    🩹 Refactor: Use pages in App

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,22 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Login, Header, Footer, Body, StudentView, InstructorView } from '#components';
+import { Header, Footer, Body } from "#components";
+import { Instructor, Login, Student } from "#pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (
-    <Router basename={`${process.env.PUBLIC_URL}/`}>
-      <Header />
-      <Body>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/student' element={<StudentView />} />
-          <Route path='/instructor' element={<InstructorView />} />
-          <Route path='*' element={<Login />} />
-        </Routes>
-      </Body>
-      <Footer />
-    </Router>
-  )
-}
+	return (
+		<Router basename={`${process.env.PUBLIC_URL}/`}>
+			<Header />
+			<Body>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/student" element={<Student />} />
+					<Route path="/instructor" element={<Instructor />} />
+					<Route path="*" element={<Login />} />
+				</Routes>
+			</Body>
+			<Footer />
+		</Router>
+	);
+};
 
-export default App
+export default App;
